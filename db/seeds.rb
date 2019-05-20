2d8a7de3009b37ed904650ca8477bcdbbaac6282
QuentinPrevost2 <prevostq@gmail.com>
2019-05-20 14:03:04 +0200

    seed of parameters

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#KPI Creation
+#Kpi.create!(label: "SG&A sur CA");
+#Kpi.create!(label: "G&A sur CA");
+#Kpi.create!(label: "Coût Finance sur CA");
+#Kpi.create!(label: "SG&A sur CA");
+#Kpi.create!(label: "Salariés gérés par ETP RH");
+#Kpi.create!(label: "Selling & Marketing cost sur CA");
+#Kpi.create!(label: "Coût Marketing sur CA");
+
+#Aproach Creation
+Approach.create!(label: "Approche globale");
+Approach.create!(label: "Approche par fonction - Finance");
+Approach.create!(label: "Approche par fonction - RH");
+Approach.create!(label: "Approche par fonction - IT");
+Approach.create!(label: "Approche par fonction - Marketing & Ventes");
+
+#Activity Creation
+Activity.create!(label: "Aérospatial & Défense");
+Activity.create!(label: "Industrie de process");
+Activity.create!(label: "Grande consommation");
+Activity.create!(label: "Luxe");
+Activity.create!(label: "Industrie petites séries");
+Activity.create!(label: "Energie et Utilities");
+Activity.create!(label: "Distribution");
+Activity.create!(label: "Private Equity");
+Activity.create!(label: "Automobile");
+Activity.create!(label: "Industrie pharmaceutique");
+Activity.create!(label: "Textile");
+Activity.create!(label: "Services");
+Activity.create!(label: "Non précisée");
+
+#Size Creation
+Size.create!(label: "CA < 50M€")
+Size.create!(label: "CA entre 50 et 250M€")
+Size.create!(label: "CA > 250M€")
+Size.create!(label: "Non précisée")
