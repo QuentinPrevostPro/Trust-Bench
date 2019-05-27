035a8385b8f05468225e5f0e44cc0de507a28d30
QuentinPrevost2 <prevostq@gmail.com>
2019-05-27 19:17:33 +0200

    seed update

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,41 +7,41 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 #KPI Creation
-Kpi.create!(label: "SG&A sur CA");
-Kpi.create!(label: "G&A sur CA");
-Kpi.create!(label: "Coût Finance sur CA");
-Kpi.create!(label: "SG&A sur CA");
-Kpi.create!(label: "Salariés gérés par ETP RH");
-Kpi.create!(label: "Selling & Marketing cost sur CA");
-Kpi.create!(label: "Coût Marketing sur CA");
+#Kpi.create!(label: "SG&A sur CA");
+#Kpi.create!(label: "G&A sur CA");
+#Kpi.create!(label: "Coût Finance sur CA");
+#Kpi.create!(label: "SG&A sur CA");
+#Kpi.create!(label: "Salariés gérés par ETP RH");
+#Kpi.create!(label: "Selling & Marketing cost sur CA");
+#Kpi.create!(label: "Coût Marketing sur CA");
 
 #Aproach Creation
-Approach.create!(label: "Approche globale");
-Approach.create!(label: "Approche par fonction - Finance");
-Approach.create!(label: "Approche par fonction - RH");
-Approach.create!(label: "Approche par fonction - IT");
-Approach.create!(label: "Approche par fonction - Marketing & Ventes");
+#Approach.create!(label: "Approche globale");
+#Approach.create!(label: "Approche par fonction - Finance");
+#Approach.create!(label: "Approche par fonction - RH");
+#Approach.create!(label: "Approche par fonction - IT");
+#Approach.create!(label: "Approche par fonction - Marketing & Ventes");
 
 #Activity Creation
-Activity.create!(label: "Aérospatial & Défense");
-Activity.create!(label: "Industrie de process");
-Activity.create!(label: "Grande consommation");
-Activity.create!(label: "Luxe");
-Activity.create!(label: "Industrie petites séries");
-Activity.create!(label: "Energie et Utilities");
-Activity.create!(label: "Distribution");
-Activity.create!(label: "Private Equity");
-Activity.create!(label: "Automobile");
-Activity.create!(label: "Industrie pharmaceutique");
-Activity.create!(label: "Textile");
-Activity.create!(label: "Services");
-Activity.create!(label: "Non précisée");
+#Activity.create!(label: "Aérospatial & Défense");
+#Activity.create!(label: "Industrie de process");
+#Activity.create!(label: "Grande consommation");
+#Activity.create!(label: "Luxe");
+#Activity.create!(label: "Industrie petites séries");
+#Activity.create!(label: "Energie et Utilities");
+#Activity.create!(label: "Distribution");
+#Activity.create!(label: "Private Equity");
+#Activity.create!(label: "Automobile");
+#Activity.create!(label: "Industrie pharmaceutique");
+#Activity.create!(label: "Textile");
+#Activity.create!(label: "Services");
+#Activity.create!(label: "Non précisée");
 
 #Size Creation
-Size.create!(label: "CA < 50M€")
-Size.create!(label: "CA entre 50 et 250M€")
-Size.create!(label: "CA > 250M€")
-Size.create!(label: "Non précisée")
+#Size.create!(label: "CA < 50M€")
+#Size.create!(label: "CA entre 50 et 250M€")
+#Size.create!(label: "CA > 250M€")
+#Size.create!(label: "Non précisée")
 
 #Bench Creation
 Bench.create!(median_value: "8.13%", min_value:"28.84%", max_value:"2.65%", source:"https://saibooks.com/sga-benchmarks/", kpi_id: 1, approach_id: 1, activity_id:6, size_id:4)
