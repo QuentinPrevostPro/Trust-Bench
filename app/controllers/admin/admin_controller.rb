e307573d9851e55e84de0c09622ae175a62831e0
QuentinPrevost2 <prevostq@gmail.com>
2019-06-18 15:43:32 +0200

    devise draft + admin part

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,9 +1,15 @@
 class Admin::AdminController < AdminController
 	def index
-		@collected_data = CollectedDatum.all
-		respond_to do |format|
-			format.html
-			format.csv { send_data @collected_data.to_csv }
-		end
+		if params[:extract] == "collect"
+			@collected_data = CollectedDatum.all
+			respond_to do |format|
+				format.csv { send_data @collected_data.to_csv, filename: "#{DateTime.now.strftime("%Y%m%d")}_historiques_données.csv" }
+			end
+		elsif params[:extract] == "bench"
+			@bench = Bench.all
+			respond_to do |format|
+				format.csv { send_data @bench.to_csv, filename: "#{DateTime.now.strftime("%Y%m%d")}benchmark.csv" }
+			end
+		end	
 	end
 end
--- app/models/bench.rb
+++ app/models/bench.rb
@@ -3,4 +3,15 @@ class Bench < ApplicationRecord
   belongs_to :approach
   belongs_to :activity
   belongs_to :size
+
+  def self.to_csv
+    attributes = %w{median_value min_value max_value source kpi_id approach_id activity_id size_id created_at}
+    CSV.generate(headers: true, col_sep: ';', encoding: 'ISO-8859-1') do |csv|
+      csv << attributes
+      all.each do |bench| 
+        csv << bench.attributes.values_at(*attributes)
+      end
+    end
+  end
+
 end
