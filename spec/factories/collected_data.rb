2ef7dd3826dcfec2c155be0855e12e0bb7a4bad4
QuentinPrevost2 <prevostq@gmail.com>
2019-07-10 19:48:05 +0200

    first model test on collected data

--- spec/factories/collected_data.rb
+++ spec/factories/collected_data.rb
@@ -1,12 +1,16 @@
 FactoryBot.define do
   factory :collected_datum do
-    value { 1.5 }
+    value { 0.1 }
     numerator { 1.5 }
-    denominator { 1.5 }
-    first_name { "MyString" }
-    last_name { "MyString" }
-    email { "MyString" }
-    company { "MyString" }
-    position { "MyString" }
+    denominator { 15 }
+    first_name { "Quentin" }
+    last_name { "Prevost" }
+    email { "quentin.prevost@argon-consult.com" }
+    company { "Argon Consulting" }
+    position { "Consultant" }
+    activity_id { FactoryBot.create(:activity).id }
+    approach_id { FactoryBot.create(:approach).id }
+    kpi_id { FactoryBot.create(:kpi).id }
+    size_id { FactoryBot.create(:size).id }
   end
 end
--- spec/models/collected_datum_spec.rb
+++ spec/models/collected_datum_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe CollectedDatum, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @collected_data = FactoryBot.create(:collected_datum)
+  end
+
+  context "validations" do
+    it "is valid with valid attributes" do
+      expect(@collected_data).to be_a(CollectedDatum)
+      expect(@collected_data).to be_valid
+    end
+  end
+
+  describe "value" do
+    it "is not valid without value" do
+      bad_data = FactoryBot.build(:collected_datum, value: nil)
+      expect(bad_data).not_to be_valid
+      expect(bad_data.errors.include?(:value)).to eq(true)
+    end
+    it "is valid when equal to numerator/denominator" do
+      expect(@collected_data.value).to eq(@collected_data.numerator/@collected_data.denominator)
+    end
+  end
+
 end
