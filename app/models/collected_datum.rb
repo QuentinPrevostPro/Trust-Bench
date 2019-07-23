2c0d03901ef81175d675c3f229aa8f6ad5834e23
QuentinPrevost2 <prevostq@gmail.com>
2019-07-23 15:11:39 +0200

    collected data extraction with label

--- app/models/collected_datum.rb
+++ app/models/collected_datum.rb
@@ -29,10 +29,15 @@ class CollectedDatum < ApplicationRecord
 
   def self.to_csv
     attributes = %w{approach_id kpi_id activity_id size_id value numerator denominator first_name last_name email company position created_at}
-    CSV.generate(headers: true, col_sep: ';', encoding: 'ISO-8859-1') do |csv|
+    CSV.generate(headers: true, col_sep: ';', encoding: 'CP1252') do |csv|
       csv << attributes
-      all.each do |collected_data| 
-        csv << collected_data.attributes.values_at(*attributes)
+      all.each do |collected_data|
+        collected_data_label = collected_data.attributes.values_at(*attributes)
+        collected_data_label[0] = Approach.all.find_by(id:collected_data.attributes.values_at(*attributes)[0]).label
+        collected_data_label[1] = Kpi.all.find_by(id:collected_data.attributes.values_at(*attributes)[1]).label
+        collected_data_label[2] = Activity.all.find_by(id:collected_data.attributes.values_at(*attributes)[2]).label
+        collected_data_label[3] = Size.all.find_by(id:collected_data.attributes.values_at(*attributes)[3]).label
+        csv << collected_data_label
       end
     end
   end
