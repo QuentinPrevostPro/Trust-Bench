871b3659f102f81f69752bab711fc7ebe9be318a
thibaultjouan <thibaultjouan@hotmail.com>
2019-09-16 17:50:35 +0200

    envoie de mail fonctionnel avec gmail

--- config/environment.rb
+++ config/environment.rb
@@ -4,11 +4,13 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
+ActionMailer::Base.delivery_method = :smtp
+
 ActionMailer::Base.smtp_settings = {
-  :user_name => ENV['EMAIL_SERVER'],
-  :password => ENV['PASSWORD_SERVER'],
-  :domain => 'argon-consult.com',
-  :address => 'smtp.office365.com',
+  :user_name => 'tibo328@gmail.com',
+  :password => 'fakeCleeAPI',
+  #:domain => 'argon-consult.com',
+  :address => 'smtp.gmail.com',
   :port => 587,
   :authentication => :login,
   :enable_starttls_auto => true
